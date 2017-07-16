1f8378c31b6d41275d94fa822d389e13038206cf
erxgithub <erxlink@gmail.com>
2017-07-16 17:50:54 -0400

    Project 2
    Added desktop responsive to CSS.

--- JS/script.js
+++ JS/script.js
@@ -18,3 +18,37 @@ var cart = {
 var addToCart = function() {
 	cart.addItem();
 };
+
+window.onload = function () {
+	document.getElementById("button1").onclick = function() {
+		addToCart();
+	};
+	
+	document.getElementById("button2").onclick = function() {
+		addToCart();
+	};
+	
+	document.getElementById("button3").onclick = function() {
+		addToCart();
+	};
+	
+	document.getElementById("button4").onclick = function() {
+		addToCart();
+	};
+	
+	document.getElementById("button5").onclick = function() {
+		addToCart();
+	};
+	
+	document.getElementById("button6").onclick = function() {
+		addToCart();
+	};
+	
+	document.getElementById("button7").onclick = function() {
+		addToCart();
+	};
+	
+	document.getElementById("button8").onclick = function() {
+		addToCart();
+	};
+}
