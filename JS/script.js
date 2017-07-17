06bad3115481e930e3b28e4324885d649aa015a1
erxgithub <erxlink@gmail.com>
2017-07-17 13:19:49 -0400

    Project 2
    Added jQuery.

--- JS/script.js
+++ JS/script.js
@@ -15,40 +15,15 @@ var cart = {
 	}
 };
 
-var addToCart = function() {
-	cart.addItem();
-};
-
 window.onload = function () {
-	document.getElementById("button1").onclick = function() {
-		addToCart();
-	};
-	
-	document.getElementById("button2").onclick = function() {
-		addToCart();
-	};
-	
-	document.getElementById("button3").onclick = function() {
-		addToCart();
-	};
-	
-	document.getElementById("button4").onclick = function() {
-		addToCart();
-	};
-	
-	document.getElementById("button5").onclick = function() {
-		addToCart();
-	};
-	
-	document.getElementById("button6").onclick = function() {
-		addToCart();
-	};
-	
-	document.getElementById("button7").onclick = function() {
-		addToCart();
-	};
-	
-	document.getElementById("button8").onclick = function() {
-		addToCart();
-	};
+    if (window.jQuery) { 
+    	console.log("jQuery loaded")
+    }
+    else {
+    	console.log("jQuery not loaded")
+    }
+
+	$(".add-button").click(function() {
+		cart.addItem();
+	});
 }
