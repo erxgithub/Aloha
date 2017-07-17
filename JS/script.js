ae764a38f8806fcd1ece5d7a888b3afab75aa795
erxgithub <erxlink@gmail.com>
2017-07-17 17:16:01 -0400

    Project 2
    Added bxSlider.

--- JS/script.js
+++ JS/script.js
@@ -26,4 +26,79 @@ window.onload = function () {
 	$(".add-button").click(function() {
 		cart.addItem();
 	});
-}
+};
+
+$(document).ready(function() {
+	var minSlides, maxSlides;
+    var width = $(window).width();
+
+	if (width < 600) {
+    	minSlides = 1;
+    	maxSlides = 1;
+	}
+	else if (width < 1240) {
+    	minSlides = 2;
+    	maxSlides = 2;
+	}
+	else {
+    	minSlides = 4;
+    	maxSlides = 4;		
+	}
+
+	$('.bxslider').bxSlider({
+        minSlides: minSlides,
+        maxSlides: maxSlides,
+        slideWidth: 390,
+		slideMargin: 10
+    });
+
+/*	$(window).resize(function () {
+		var minSlides, maxSlides;
+    	var width = $(window).width();
+
+		if (width < 600) {
+	    	minSlides = 1;
+	    	maxSlides = 1;
+		}
+		else if (width < 1240) {
+	    	minSlides = 2;
+	    	maxSlides = 2;
+		}
+		else {
+	    	minSlides = 4;
+	    	maxSlides = 4;		
+		}
+
+		var slider = $('.bxslider').bxSlider();
+
+		//$('.bxslider').bxslider.reloadSlider({
+		slider.reloadSlider({
+	        minSlides: minSlides,
+	        maxSlides: maxSlides,
+	        slideWidth: 390,
+			slideMargin: 10
+	    });
+
+	    console.log("resize");
+	});
+*/
+	//var slider = $('.bxslider').bxSlider();
+	//var widthMatch = matchMedia("only screen and (min-width: 600px) and (max-width: 1239px))");
+    //var widthHandler = function(matchList) {
+    //    if (matchList.matches) {
+    //        slider.reloadSlider({
+    //            minSlides: 1,
+    //            maxSlides: 1,
+    //            slideWidth: 390,
+  	//			slideMargin: 10
+    //        })
+    //    } else {
+    //        slider.reloadSlider({
+    //            minSlides: 4,
+    //            maxSlides: 4,
+    //            slideWidth: 390,
+  	//			slideMargin: 10
+    //        })
+    //    }
+    //};
+});
